e51475a420dc6cf36cdebaeaa222d7476d22f39c
Abdul-Ahad-CSE <abdulahad.cse1@gmail.com>
2025-10-27 19:43:08 +0600

    Dragon News with firebase

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LatestNews() {
+  return (
+    <div className='flex items-center gap-5 bg-base-200 p-3'>
+        <p className='text-base-100 bg-secondary px-3 py-2'>Latest</p>
+        <p className='font-bold'>Lorem  sit, amet consectetur adipisicing elit. Dignissimos autem, dicta voluptatibus maxime beatae nemo?</p>
+    </div>
+  )
+}
